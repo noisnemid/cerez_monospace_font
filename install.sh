06c6247d4458b531523ec3c89f12fe55534acb23
noisnemid <16525011+noisnemid@users.noreply.github.com>
2022-08-25 09:38:01 +0800

    refresh crerx

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-mv ~/.local/share/fonts/cerez.* ~/trash
+mkdir -p ~/.local/share/fonts/
+mkdir -p ~/.trash_manual
+mv ~/.local/share/fonts/cerez.* ~/.trash_manual
 cp cerez.*tf ~/.local/share/fonts/
 # fc-cache -fv
 python ./fontforge_x_version_font_generate.py
